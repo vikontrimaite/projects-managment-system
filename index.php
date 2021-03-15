bc6b89b1dfec22926a3de773b9bc358530c161d0
Vilija Kontrimaite <vikontrimaite@gmail.com>
2021-03-15 18:01:23 +0200

    update style

--- index.php
+++ index.php
@@ -17,13 +17,17 @@ include('head.php');
 include('header.php');
 
    ?>
-
-   <h1>Hello! Please choose which table you would like to see:</h1>
-   <p>
+<div class="index-page">
+<h1>Hello! Please choose which table you would like to see:</h1>
+   
+<p>
     <a href="projects.php" class="links">projects</a>
-    or
+    <span>or</span>
                 <a href="employees.php" class="links">employees</a>
    </p>
+
+</div>
+  
 
 
 </body>
