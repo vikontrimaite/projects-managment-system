afa7686af4acd4077f8ac860ceed02a8fa507183
Vilija Kontrimaite <vikontrimaite@gmail.com>
2021-03-16 15:44:51 +0200

    delete employee

--- employees.php
+++ employees.php
@@ -14,6 +14,32 @@ include('head.php');
 
 include('header.php');
 
+// delete
+if(isset($_GET['action']) and $_GET['action'] == 'delete'){
+    $sql = 'DELETE FROM Employees WHERE id = ?';
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $_GET['id']);
+    $res = $stmt->execute();
+
+    $stmt->close();
+    mysqli_close($conn);
+
+    header("Location: " . strtok($_SERVER["REQUEST_URI"], '?'));
+    die();
+}
+
+
+// čia pagal seną pvz, bet reik kitaip, bet esmė gal tokia
+// if(isset($_POST['add-empl'])) {
+//     $newDir = $_GET['path']. $_POST['add-empl'];
+//     if (is_dir($_POST['add-empl'])) {
+//         echo '<p style="color: red;">Employee with a name ' . $_POST['add-empl'] . ' exists aleready</p>';
+//     } elseif (empty($_POST['add-empl'])) {
+//         echo '<p style="color: red;">Employee name cannot be empty. Please enter a new employee name!</p>';
+//     } else {
+//         mkdir($newDir);
+//     }
+// }
 
 echo '<table>
             <tr>
@@ -33,7 +59,9 @@ if (mysqli_num_rows($result) > 0) {
             <td>' . $row["id"] . '</td>
             <td>' . $row["employee_name"] . '</td>
             <td>' . $row["project_name"] . '</td>
-            <td>delete update</td>
+            <td>
+            <a href="?action=delete&id='  . $row['id'] . '"><button>DELETE</button></a>
+            update</td>
         </tr>';
     }
 } else {
@@ -43,6 +71,16 @@ if (mysqli_num_rows($result) > 0) {
 echo '</table>';
 
 ?>
+
+<div style="background-color: violet;">
+        <form action="" method="POST">
+            <p>Add a new employee!</p>
+            <label for="add-empl">Enter a new employee's name: </label>
+            <input type="text" name="add-empl">
+            <input type="submit" value="Create">
+        </form>
+    </div>
+
 </body>
 </html>
 
